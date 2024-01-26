81c70acd1750ac4e42e2c49c7bff9abde6cfb252
Moheed789 <moheedsiddiqui74@gmail.com>
2024-01-25 16:38:22 -0800

    Generate CRUD Api and use in middleware

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,19 @@
-export async function GET() {
-    return new Response("Profile API Data");
+import { type NextRequest } from "next/server";
+import { headers, cookies } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers();
+    cookies().set("resultPerPage", "20");
+    const theme = request.cookies.get("theme");
+    console.log("token", requestHeaders.get("Authorization"));
+    console.log("Token", headerList.get("Authorization"));
+    console.log("Theme", theme)
+    console.log("ResultPerPage", cookies().get("resultPerPage"))
+    return new Response("<h1>Profile API Data</h1>", {
+        headers: {
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
+        }
+    });
 }
